8c863b8542941cfb4e2b39cb71c19ac97980e981
Ashutosh Kumar Mourya <ashutoshkumarmourya90@gmail.com>
2026-07-09 23:08:01 +0530

    Create the_Area_of_a_circle.java

---
+++ the_Area_of_a_circle.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args)
+	{
+			
+		double radius = 7;			
+		
+        //formula for area of a circle
+		double area = 3.14 * radius * radius;		
+		System.out.println(area);
+		
+	}
+}
